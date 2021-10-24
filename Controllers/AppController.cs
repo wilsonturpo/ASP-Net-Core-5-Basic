53fc90c38e8f8162b022a0c014c3257a181e6277
wilsonturpo <wilson.turpo@tecsup.edu.pe>
2021-10-24 13:32:46 -0500

    CRUD Finished - without DB

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using MovieApp.ViewModel;
+using System.Linq;
 
 namespace MovieApp.Controllers{
     public class AppController : Controller{
@@ -12,22 +13,37 @@ namespace MovieApp.Controllers{
             return View(_movies);
         }
 
-        public IActionResult AddOrEdit(){
-            return View();
+        public IActionResult AddOrEdit(Guid id){
+            var movie = _movies.FirstOrDefault(x => x.Id == id);
+            return View(movie);
         }
 
         [HttpPost]
         public IActionResult AddOrEdit(MovieViewModel model){
+            
+            var movie = _movies.FirstOrDefault(x => x.Id == model.Id);            
+
             if(ModelState.IsValid){
-                _movies.Add(model);
+                if(movie == null){
+                    model.Id = Guid.NewGuid();
+                    _movies.Add(model);                    
+                }else{
+                    movie.Genre = model.Genre;
+                    movie.Name = model.Name;
+                }
                 return RedirectToAction(nameof(Index));
             }
             return View();
         }
 
+        public IActionResult Delete(Guid id){
+            _movies.Remove(_movies.FirstOrDefault(x => x.Id == id));
+            return RedirectToAction(nameof(Index));
+        }
+
         [HttpGet("About")]
         public IActionResult About(){
-            throw new InvalidOperationException();
+            //throw new InvalidOperationException();
             return View();
         }
     }
--- ViewModel/MovieViewModel.cs
+++ ViewModel/MovieViewModel.cs
@@ -1,7 +1,11 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace MovieApp.ViewModel{
     public class MovieViewModel{
+
+        public Guid Id {get; set;}
+
         [Required(ErrorMessage ="El Nombre es requerido")]
         [MaxLength(30, ErrorMessage ="Texto demasiado largo")]
         public string Name {get; set;}
